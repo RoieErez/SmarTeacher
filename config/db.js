e7a82965fb3809a5979f05979a52d67a4edde86b
Roie Erez <roieerez110@gmail.com>
2019-10-06 22:43:10 +0300

    jwt implement

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoos = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoos.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('mongoDB Connected....');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//connecting DB
+connectDB();
+
+//init mid ware
+app.use(express.json({ exteended: false }));
 
 app.get('/', (req, res) => {
   res.send('API running');
 });
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.POST || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
